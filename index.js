6caec3aa93aeca9a415ddb237798775e6ac67a79
minhhoccode111 <minhhoccode111@gmail.com>
2022-12-15 02:14:27 +0700

    logic to add books to library

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 console.log("hello world from html");
 
+const wrapper = document.querySelector(".wrapper");
 const plusButton = document.querySelector(".add-button");
 const formContainer = document.querySelector(".form-container");
 const haveReadButton = document.querySelector(".switch");
@@ -10,25 +11,29 @@ const titleInput = document.querySelector(".title");
 const authorInput = document.querySelector(".author");
 const totalInput = document.querySelector(".total-pages");
 const completedInput = document.querySelector(".completed-pages");
+const warnText = document.querySelector(".warn");
 
 plusButton.onclick = () => formContainer.classList.remove("none");
 cancelButton.onclick = () => formContainer.classList.add("none");
 haveReadButton.onclick = () => (completedInput.value = totalInput.value);
+completedInput.onkeyup = checkCompletedPages; //check to make sure the pages you've read would not be higher than the book's pages
+completedInput.onchange = checkCompletedPages; // ot check if you didn't fill the book's pages first
 addButton.onclick = handleAddButton;
 
 let myLibrary = [];
 let book;
 
 function handleAddButton(e) {
-  // inputs.forEach((input) => console.log(input.value));
   if (
     titleInput.value === "" ||
     authorInput.value === "" ||
     totalInput.value == "" ||
-    completedInput == ""
+    completedInput.value == ""
   ) {
-    return;
+    warnText.classList.remove("none"); // show warn text
+    return; //do nothing when we have an empty field
   }
+  warnText.classList.add("none"); //remove warn text
   book = new Book(
     titleInput.value,
     authorInput.value,
@@ -37,6 +42,9 @@ function handleAddButton(e) {
   );
   myLibrary.push(book);
   console.log(myLibrary);
+  formContainer.classList.add("none");
+  resetBookForm();
+  return myLibrary;
 }
 
 function Book(title, author, total, completed) {
@@ -44,11 +52,32 @@ function Book(title, author, total, completed) {
   this.author = author;
   this.total = total;
   this.completed = completed;
+  showBooks(title, author, total, completed);
   //the constructor
 }
 
-function addBookToLibrary() {
-  //do something
+function resetBookForm() {
+  [
+    titleInput.value,
+    authorInput.value,
+    totalInput.value,
+    completedInput.value,
+  ] = ["", "", "", ""];
 }
 
-function showBooksInLibrary() {}
+function showBooks(a, b, c, d) {
+  const newBook = document.createElement("div");
+  newBook.classList.add("book-wrapper");
+  newBook.innerHTML = ``;
+  wrapper.appendChild(newBook);
+  console.log(a, b, c, d);
+}
+
+function checkCompletedPages() {
+  let a = Number(totalInput.value);
+  let b = Number(completedInput.value);
+  if (a == "" || b > a) {
+    completedInput.value = totalInput.value;
+    return;
+  }
+}
